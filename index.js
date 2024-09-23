48a8c6e3456646619c840fa017fa1493cf7a44ae
Jon Desserres <jondesserres@gmail.com>
2024-09-23 12:13:48 -0700

    created an image carousel section, added some optimized images for the carousel, and added some a js script to handle the carousel function

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
+// Hamburger Menu Function
 function toggleMenu() {
     const navLinks = document.getElementById('navLinks');
     navLinks.classList.toggle('show');
-}  
+}
+
+// Carousel
+const carousel = document.getElementById('carousel');
+let currentIndex = 0;
+const slides = document.querySelectorAll('.carousel-slide');
+const totalSlides = slides.length;
+
+function showNextSlide() {
+    currentIndex++;
+    if (currentIndex >= totalSlides) {
+        currentIndex = 0;
+    }
+    const translateX = -currentIndex * 100; // Slide to the next image
+    carousel.style.transform = `translateX(${translateX}%)`;
+}
+
+// Auto-slide every 5 seconds
+setInterval(showNextSlide, 4000);
